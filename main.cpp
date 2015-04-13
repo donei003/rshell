6ccac01c4e3656efa851826474d6afe8dfd38975
Dylan O'Neill <donei003@ucr.edu>
2015-04-13 04:00:56 -0700

    commands with no arguements work now

--- main.cpp
+++ main.cpp
@@ -21,50 +21,40 @@ if(argc < 1) {
 }
 else {
     int currArg = 1;
+    int SAP = 0; // Sub-Array Position
     string ArgStr;
-    cout << argv[1] << endl;
-    cout << *(argv[1] + 1) << endl;
     char* test = argv[1];
-    while(currArg+1 < argc) {
-        arg[0] = argv[currArg];
+    while(currArg+1 <= argc) {
+        SAP = 0;
+        arg[SAP] = argv[currArg];
+        ++SAP;
         int n = 0;
         for(int i = 0; i < strlen(argv[currArg]); ++i) {
-            if(*(argv[currArg+1]+i) == ';' && i == (strlen(argv[currArg])-1)){
-                *(argv[currArg+1]+i) = '\0';
+            if(*(argv[currArg]+i) == ';' && i == (strlen(argv[currArg])-1)){
+                *(argv[currArg]+i) = '\0';
                 isDependant = 0;
             }
         }
-        arg[1] = argv[currArg+1];
         ++currArg;
-    }
-    arg[2] = NULL;
-    /*
-    while(currArg < argc){
-        if(*(argv[currArg]).at(0) = '-') {
-            if(*(argv[currArg]).at(*(argv[currArg]).size() - 1) == ';') {
-                *argv[currArg].pop_back();
-                endCommand = 1;
-            }
-            ArgStr += argv[currArg];
+        if((currArg) < argc && (argv[currArg]) != "||" && (argv[currArg]) != "&&") {
+            arg[SAP] = argv[currArg];
+            ++SAP;
         }
+        else {
+            arg[SAP] = NULL;
+            ++SAP;
+        }
+        arg[SAP] = NULL;
+        char *testa[3] = { NULL};
+        if(execvp(arg[0], arg) == -1) {
+            perror("The command could not be executed!");
+            errno = 0;
+        }
+        /*else {
+            execvp(arg[0], arg);
+        }*/
         ++currArg;
-
-
-
-    }*/
-    execvp(arg[0], arg);
-    //cout << get_current_dir_name() << endl;
-    /*DIR *dirp;
-    if( NULL == (dirp = opendir(get_current_dir_name()))) {
-      perror("The directory could not be opened or does not exist. ");
-      exit(1);
     }
-    struct dirent *file;
-    errno = 0;
-    while(NULL != (file = readdir(dirp))) {
-      cout << file->d_name << " ";
-    }
-    cout << endl;*/
   }
 return 0;
 }
