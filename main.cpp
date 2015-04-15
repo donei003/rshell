3542a6c59a0b109b4daaf8313dde64b65ff1a4dc
Dylan O'Neill <donei003@ucr.edu>
2015-04-14 22:28:06 -0700

    re-wrote main.cpp, misunderstood assignment

--- main.cpp
+++ main.cpp
@@ -13,48 +13,29 @@ void lsCom() {
 
 }
 bool isDependant = 0;
-int main(int argc, char *argv[]) {
-char *arg[argc];
-if(argc < 1) {
-    cout << "No arguements passed in" << endl;
-    exit(1);
-}
-else {
-    int currArg = 1;
-    int SAP = 0; // Sub-Array Position
-    string ArgStr;
-    char* test = argv[1];
-    while(currArg+1 <= argc) {
-        SAP = 0;
-        arg[SAP] = argv[currArg];
-        ++SAP;
-        int n = 0;
-        for(int i = 0; i < strlen(argv[currArg]); ++i) {
-            if(*(argv[currArg]+i) == ';' && i == (strlen(argv[currArg])-1)){
-                *(argv[currArg]+i) = '\0';
-                isDependant = 0;
+int main() {
+    string str;
+    int strSize = 0;
+    int pos;
+    while(1) {
+        getline(cin, str);
+        pos = 0;
+        strSize = str.size();
+        while(pos < strSize) {
+            if(str.at(pos) == ' ') {
+                while(pos < strSize && str.at(pos) == ' ') {
+                    ++pos;
+                }
+                if(pos >= strSize) {
+                    break;
+                }
             }
+            while(str.at(pos) != ' ') {
+
+            }
+            execvp();
         }
-        ++currArg;
-        if((currArg) < argc && (argv[currArg]) != "||" && (argv[currArg]) != "&&") {
-            arg[SAP] = argv[currArg];
-            ++SAP;
-        }
-        else {
-            arg[SAP] = NULL;
-            ++SAP;
-        }
-        arg[SAP] = NULL;
-        char *testa[3] = { NULL};
-        if(execvp(arg[0], arg) == -1) {
-            perror("The command could not be executed!");
-            errno = 0;
-        }
-        /*else {
-            execvp(arg[0], arg);
-        }*/
-        ++currArg;
     }
-  }
+
 return 0;
 }
