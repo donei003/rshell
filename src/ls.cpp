cd56b714dbda0097ffb9b1e302ef0cef9f27c47e
Dylan O'Neill <donei003@ucr.edu>
2015-04-27 00:22:07 -0700

    Some progress, stupid ass seg fault

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,57 +1,70 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 using namespace std;
 
 bool noArg = false, flagA = false, flagL = false, flagR = false;
-int argn = 0;
-char* arga[];
-void findArg() {
+void findArg(int argc, char* argv[]) {
     if(!(noArg)) {
         int ArrInd = 1, ArrPos = 0;
-        while(ArrInd < argc && *(argv[ArrInd]) == '-') {
-            ArrPos = 0;
-            while(*(argv[ArrInd] + ArrPos) != '\0') {
-                if(*(argv[ArrInd] + ArrPos) == 'a') {
-                    flagA = true;
+        while(ArrInd < argc) {
+            if(*(argv[ArrInd]) == '-') {
+                ArrPos = 0;
+                while(*(argv[ArrInd] + ArrPos) != '\0') {
+                    if(*(argv[ArrInd] + ArrPos) == 'a') {
+                        flagA = true;
+                    }
+                    else if(*(argv[ArrInd] + ArrPos) == 'l') {
+                        flagL = true;
+                    }
+                    else if(*(argv[ArrInd] + ArrPos) == 'R') {
+                        flagR = true;
+                    }
+                    ++ArrPos;
                 }
-                else if(*(argv[ArrInd] + ArrPos) == 'l') {
-                    flagL = true;
-                }
-                else if(*(argv[ArrInd] + ArrPos) == 'R') {
-                    flagR = true;
-                }
-                ++ArrPos;
+                ++ArrInd;
             }
-            ++ArrInd;
+
+            else {
+                ++ArrInd;
+            }
         }
     }
 }
 
-            
-
 
 int main(int argc, char* argv[]) {
-    argn = argc;
-    arga[] = argv[];
-    
+    int size = PATH_MAX;
+    struct dirent *cDirent;
+    DIR *cDir;
     if(argc == 1) {
         noArg = true;
+
+    }
+    char* pathname = getcwd(pathname, (size_t)size);
+    cout << pathname << endl;
+    cDir = opendir(pathname);
+    if(cDir == NULL) {
+        perror("It didn't work");
+        errno = 0;
+        return 1;
     }
 
-    /*int n = 0;
-    cout << argc << endl;
-    while(*(argv[2]+n) != '\0') {
-        if(*(argv[2] + n) == ' ') {
-            cout << "-";
-        }
-        else {
-            cout << *(argv[2] + n);
-        }
-        ++n; 
+    while((cDirent = readdir(cDir)) != NULL) {
+        cout << cDirent->d_name << endl;
     }
-    cout << endl;*/
+    closedir(cDir);
+
+    /*struct stat s;
+    stat("ls.cpp", &s);*/
 
     
-    findArg();
+    findArg(argc, argv);
 
     if(flagA) {cout << "1" << endl;}
     if(flagL) {cout << "2" << endl;}
